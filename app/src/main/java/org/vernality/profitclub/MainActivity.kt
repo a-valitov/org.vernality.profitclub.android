9db999c1a15fc51c6053b2655218cb3fdc0efeaa
AleksandrHub <masterAlexandr@yandex.ru>
2020-10-18 19:01:01 +0300

    added root

---
+++ app/src/main/java/org/vernality/profitclub/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.vernality.profitclub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
